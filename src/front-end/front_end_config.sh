40dd806ea1bc8e9dc586ec87cea16991abd6fc7e
RajatG17 <68469636+RajatG17@users.noreply.github.com>
2023-05-02 18:40:46 -0400

    Added shell config files

---
+++ src/front-end/front_end_config.sh
@@ -0,0 +1,8 @@
+export CATALOG_HOST="0.0.0.0"
+export ORDER_HOST="0.0.0.0"
+export FRONTEND_HOST="0.0.0.0"
+export FRONTEND_PORT=8000
+export CACHE_SIZE=5
+export ORDER_ID="5,6,8"
+export ORDER_PORTS="6001,6002,6003"
+export ORDER_HOSTS="0.0.0.0,0.0.0.0,0.0.0.0"
---
+++ src/order/order_config_3.sh
@@ -0,0 +1,9 @@
+export SERVICE_ID="8"
+export ORDER_HOST="0.0.0.0" 
+export ORDER_PORT=6003 
+export CATALOG_HOST="0.0.0.0" 
+export CATALOG_PORT=6000
+export ORDER_ID="5,6,8" 
+export FILE_PATH="../data/" 
+export ORDER_PORTS="6001,6002,6003"
+export ORDER_HOSTS="0.0.0.0,0.0.0.0,0.0.0.0"
